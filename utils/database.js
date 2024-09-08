92dbdfc249837f8252ff5839c16575774744200f
Georgi Stankov <vangog06406@gmail.com>
2024-09-08 21:28:31 +0300

    Fix mongodb connection

--- utils/database.js
+++ utils/database.js
@@ -2,24 +2,28 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
+const clientOptions = {
+  serverApi: {
+    version: "1",
+    strict: true,
+    deprecationErrors: true,
+  },
+};
+
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (isConnected) {
     console.log("MongoDB is already connected");
     return;
-  } else {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI, {
-        dbName: "share_prompt",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+  }
 
-      isConnected = true;
-      console.log("MongoDB connected");
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, clientOptions);
+
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
   }
 };
